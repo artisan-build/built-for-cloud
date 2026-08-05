ebe15d71f06b75e9177c5f060b8ac3620327e855
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2026-08-05 15:19:39 +0200

    Set version to 0.3.0 (#16)
    Co-authored-by: Ed Grosvenor <ed@artisan.build>

--- src/BuiltForCloud.php
+++ src/BuiltForCloud.php
@@ -6,7 +6,7 @@ namespace ArtisanBuild\BuiltForCloud;
 
 final class BuiltForCloud
 {
-    public const VERSION = '0.1.x';
+    public const VERSION = '0.3.0';
 
     public const API_VERSION = 1;
 }
