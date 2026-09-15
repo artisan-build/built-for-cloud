926d6786d501011d47d1da5e7e7a44b491d871b8
Ed Grosvenor <ed@artisan.build>
2026-09-15 11:50:09 +0200

    Bump version to 0.12.1

--- src/BuiltForCloud.php
+++ src/BuiltForCloud.php
@@ -6,7 +6,7 @@ namespace ArtisanBuild\BuiltForCloud;
 
 final class BuiltForCloud
 {
-    public const VERSION = '0.12.0';
+    public const VERSION = '0.12.1';
 
     /**
      * The public HTTP contract's major version (docs/http-contract.md, PRD
